8e6b59bbaae5430c7f6b8ffc39fcd0a5411045e4
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-11-23 00:53:23 +0900

    add: streamlit-elementsのdemoを参考にmui.Cardを使った雛形を試作

--- index.py
+++ index.py
@@ -7,3 +7,46 @@ st.set_page_config(
     layout="wide",
 )
 
+
+from streamlit_elements import elements, dashboard, mui, media
+
+
+with st.sidebar:
+    st.title("🗓️ #30DaysOfStreamlit")
+    st.header("Day 27 - Streamlit Elements")
+    st.write("Build a draggable and resizable dashboard with Streamlit Elements.")
+    st.write("---")
+
+    # Define URL for media player.
+    media_url = st.text_input("Media URL", value="https://www.youtube.com/watch?v=vIQQR_yq-8I")
+
+
+layout = [
+    # Editor item is positioned in coordinates x=0 and y=0, and takes 6/12 columns and has a height of 3.
+    dashboard.Item("editor", 0, 0, 6, 3),
+    # Media item is positioned in coordinates x=0 and y=2, and takes 12/12 columns and has a height of 4 and is draggable and resizable.
+    dashboard.Item("media", 0, 1, 12, 4, isDraggable=True, isResizable=True, moved=False)
+]
+
+
+with elements("demo"):
+
+    with dashboard.Grid(layout):
+
+        with mui.Card(key="editor", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Media Player", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+
+                # This element is powered by ReactPlayer, it supports many more players other
+                # than YouTube. You can check it out there: https://github.com/cookpete/react-player#props
+
+                media.Player(url=media_url, width="100%", height="100%", controls=True)
+
+        with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="Media Player", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+
+                # This element is powered by ReactPlayer, it supports many more players other
+                # than YouTube. You can check it out there: https://github.com/cookpete/react-player#props
+
+                media.Player(url=media_url, width="100%", height="100%", controls=True)
