2fc8cabb8a734a015caf5dcbb8d5f89f93f204da
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-11-28 11:18:46 +0900

    update: cardとlayoutの雛形を作成・動作確認

--- components/card_demo.py
+++ components/card_demo.py
@@ -1,13 +1,35 @@
 # Cardのパラメータを定義するファイル
 
-from streamlit_elements import mui
+from streamlit_elements import mui, media
 
+# * デモ用のカード
 def card_demo(kw):
     with mui.Card(key=kw, sx={"display": "flex", "flexDirection": "column"}):
-                mui.CardHeader(title="Media Player", className="draggable")
-                with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+        mui.CardHeader(title="Media Player")
 
-                    # This element is powered by ReactPlayer, it supports many more players other
-                    # than YouTube. You can check it out there: https://github.com/cookpete/react-player#props
+        with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+            mui.Typography("Hello world")
 
-                    mui.Typography("Hello world")
+
+# * Text カード
+def card_text(kw):
+    with mui.Card(key=kw, sx={"display": "flex", "flexDirection": "column"}):
+        mui.CardHeader(title="Text")
+
+
+# * Image カード
+def card_image(kw):
+    with mui.Card(key=kw, sx={"display": "flex", "flexDirection": "column"}):
+        mui.CardHeader(title="Image")
+
+
+# * Video カード
+def card_video(kw):
+    with mui.Card(key=kw, sx={"display": "flex", "flexDirection": "column"}):
+        mui.CardHeader(title="Video")
+
+
+# * Graph カード
+def card_graph(kw):
+    with mui.Card(key=kw, sx={"display": "flex", "flexDirection": "column"}):
+        mui.CardHeader(title="Graph")
--- index.py
+++ index.py
@@ -1,32 +1,47 @@
 import streamlit as st
 
 # ページ設定
-st.set_page_config(
-    page_title="Adaptive UI Prototype",
-    page_icon="🍴",
-    layout="wide",
-)
+def set_page_config():
+    st.set_page_config(
+        page_title="Adaptive UI Prototype",
+        page_icon="🍴",
+        layout="wide",
+    )
 
 
-from streamlit_elements import elements, dashboard, mui
+from streamlit_elements import elements, dashboard
 
-
-with st.sidebar:
-    st.title("🗓️ #30DaysOfStreamlit")
-    st.header("Day 27 - Streamlit Elements")
-    st.write("Build a draggable and resizable dashboard with Streamlit Elements.")
-    st.write("---")
-
-    # Define URL for media player.
-    media_url = st.text_input("Media URL", value="https://www.youtube.com/watch?v=vIQQR_yq-8I")
+def view_sidebar():
+    with st.sidebar:
+        st.title("🗓️ #30DaysOfStreamlit")
+        st.header("Day 27 - Streamlit Elements")
+        st.write("Build a draggable and resizable dashboard with Streamlit Elements.")
+        st.write("---")
 
 
 import components.layout as ll
 import components.card_demo as ccd
 
-with elements("demo"):
+def view_contents():
 
-    with dashboard.Grid(ll.layout_demo):
+    with elements("contents"):
 
-        ccd.card_demo('media')
-        ccd.card_demo('editor')
+        # with dashboard.Grid(ll.layout_single_column):
+        with dashboard.Grid(ll.layout_multi_column):
+        # with dashboard.Grid(ll.layout_grid):
+
+            ccd.card_text('text')
+            ccd.card_image('image')
+            ccd.card_video('video')
+            ccd.card_graph('graph')
+
+
+def main():
+    set_page_config()
+    view_sidebar()
+    view_contents()
+
+
+
+if __name__ == '__main__':
+    main()
